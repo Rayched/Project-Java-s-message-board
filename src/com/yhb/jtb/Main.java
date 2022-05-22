c7a881d6c751dcfd7fafd4b83353bbac632d1995
Rayched <ny2401@naver.com>
2022-05-22 12:31:05 +0900

    build 0.1

---
+++ src/com/yhb/jtb/Main.java
@@ -0,0 +1,11 @@
+package com.yhb.jtb;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("=== 게시판 v 0.1 ===");
+		System.out.println("=== 프로그램 시작 ===");
+		System.out.println("명령) ");
+		System.out.println("=== 프로그램 종료 ===");
+	}
+}
