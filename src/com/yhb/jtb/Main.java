d5deaeaf4a224a42f49a6efce1fd2b49dad8b236
Rayched <ny2401@naver.com>
2022-06-01 15:56:14 +0900

    build 0.3.2

--- src/com/yhb/jtb/Main.java
+++ src/com/yhb/jtb/Main.java
@@ -9,6 +9,8 @@ public class Main {
 		//여기서 System.in은 사용자로 부터 명령을 입력받기 위한 입력 스트림이다.
 		System.out.println("=== 게시판 v 0.3 ===");
 		System.out.println("=== 프로그램 시작 ===");
+		int LastId = 0; //정수형 변수 Lastid 선언, 마지막에 작성한 게시글을 의미
+		//현재 작성한 게시물의 수가 0개이므로 값도 0으로 설정
 		while (true) { //명령어를 계속 입력해야 하므로 반복문인 while문을 사용
 			System.out.printf("명령) ");
 			String cmd = sc.nextLine(); //String 형 입력 및 리턴
@@ -19,8 +21,10 @@ public class Main {
 				System.out.printf("제목) ");
 				String title = sc.nextLine(); //입력된 명령을 title 변수에 저장
 				System.out.printf("내용) ");
-				String body = sc.nextLine(); // 입력된 명령을 body 변수에 저장
-				System.out.println("1번 게시물이 등록되었습니다.");
+				String body = sc.nextLine(); // 입력된 명령을 body 변수에 저장	
+				int id = LastId + 1; //변수 id 선언, 현재 게시물을 의미함, LastId 초기값에 1을 더함
+				LastId = id; //id에 저장된 값을 LastId에 덮어쓰기함, 마지막으로 작성한 게시글의 수
+				System.out.printf("%d번 게시물이 등록되었습니다.\n", id);
 			}
 			else if (cmd.equals("exit")) {//조건: cmd에 입력된 명령어가 exit일 때 true
 				break; //exit 명령어가 입력되면 while 반복문을 종료시킴
